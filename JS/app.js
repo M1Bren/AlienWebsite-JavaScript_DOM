71bb1c310cb6d6d75145d5cf633be6e92853d1ad
M1Bren <blaw908@gmail.com>
2019-11-21 04:34:26 -0500

    update

--- JS/app.js
+++ JS/app.js
@@ -1,30 +1,34 @@
-// from data.js
-var originalData = dataset;
-
 // Define variables connecting to the index.html input bars and buttons
 var tbody = d3.select("#tbody");
-var dateInput = d3.select("#date");
+var dateInput = d3.select("#datetime");
 var cityInput = d3.select("#city");
 var stateInput = d3.select("#state");
 var countryInput = d3.select("#country");
 var shapeInput = d3.select("#shape");
-var searchButton = d3.select("#filter-btn").on("click", searchData); // activates button with corresponding function on a 'click'
+var searchButton = d3.select("#filter-btn");
 var resetButton = d3.select("#reset");
+
+// Event Listener(s)
+searchButton.on("click", searchData);
 
 // Index and result page numbers
 var startIndex = 0;
 var resultsNumber = 100;
 
-function searchData(event) {
+function searchData() {
     // prevent the page from refreshing during a search
     d3.event.preventDefault();
 
-    var searchDate = dateInput.value
-    if (searchDate != "") {
-        var filteredData = originalData.filter(function(data) {
-            var dataDate = data.datetime;
-            console.log(dataDate);
-            return dataDate === filteredData;
+    // Return a 'filteredData' table with dataset indices matching user-imput datetime 
+    var searchDate = dateInput.value;
+    console.log(searchDate)
+    if (searchDate !== "") {
+        var filteredData = dataset.filter(datum => {
+            
+            return datum.datetime === searchDate;
         })
-    }
+    };
+    console.log(filteredData);
 };
+
+
