f2b4a5089d30b18d58a1d4c1a8033c9019a73fa6
M1Bren <blaw908@gmail.com>
2019-11-23 12:21:24 -0500

    final v1

--- JS/app.js
+++ JS/app.js
@@ -16,20 +16,24 @@ d3.selectAll("#filter-btn").on("click", searchData);
 
 // Function to create a table to display on the webpage
 function createTable(tableData) {
-    var tBody = d3.select("#tbody");
-    tableData.forEach(function(load_data) {
-        var tRow = tBody.append("tr");
-        tRow.append("td").text(tableData.datetime);
-        tRow.append("td").text(tableData.city);
-        tRow.append("td").text(tableData.state);
-        tRow.append("td").text(tableData.country);
-        tRow.append("td").text(tableData.shape);
-        tRow.append("td").text(tableData.durationMinutes);
-        tRow.append("td").text(tableData.comments);
+    d3.select("tbody")
+    .selectAll("tr")
+    .data(tableData)
+    .enter()
+    .append("tr")
+    .html(d => `<td>${d.datetime}</td>
+                <td>${d.city}</td>
+                <td>${d.state}</td>
+                <td>${d.country}</td>
+                <td>${d.shape}</td>
+                <td>${d.durationMinutes}</td>
+                <td>${d.comments}</td>`);
+    
+    // More modular way to create the table in HTML below
         // Object.entries(load_data).forEach(function([value]) {
         //     tBody.append("td").text(value);
         // })
-    });
+    // });
 }
 
 // Function to read input data on button click and use it to filter tableData for createTable
@@ -41,8 +45,12 @@ function searchData() {
     var dateInput = d3.select("#datetime").property("value");
 
     if (dateInput !== "") {
-        var filterData = tableData.filter(datum => (datum.datetime === dateInput));
+        var filterData = tableData.filter(d => (d.datetime === dateInput));
         }
+        console.log(dateInput);
         console.log(filterData);
         createTable(filterData);        
     }
+
+// load the entire table when the webpage opens
+createTable(tableData)
